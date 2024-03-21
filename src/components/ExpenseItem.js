6baee99e7cf0702402630699c493455243219e5a
Joseph Walker <avgwalker12@gmail.com>
2024-03-21 19:35:30 -0400

    added shopping app in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
 
+// eslint-disable-next-line
 const ExpenseItem = (props) => {
 };
 
